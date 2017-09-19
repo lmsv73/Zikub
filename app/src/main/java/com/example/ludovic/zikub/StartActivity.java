c11f5a1c3ca8fb3a7cb796370e21817ce6f5af93
victorbasset <victor.basset@yahoo.fr>
2017-09-19 17:30:14 +0200

    Bouton mis à jour

--- app/src/main/java/com/example/ludovic/zikub/StartActivity.java
+++ app/src/main/java/com/example/ludovic/zikub/StartActivity.java
@@ -1,23 +1,26 @@
 package com.example.ludovic.zikub;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class StartActivity extends Activity implements View.OnClickListener{
+public class StartActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        final Button button = (Button) findViewById(R.id.email_sign_in_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(StartActivity.this, LoginActivity.class);
+                StartActivity.this.startActivity(myIntent);
+            }
+        });
     }
-
-    @Override
-    public void onClick(View v) {
-        setContentView(R.layout.activity_login);
-    }
-
-
 }
 
