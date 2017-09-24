01d1907083e50c389c3a7683a6963adad55c407c
Victor <victor.basset@yahoo.fr>
2017-09-24 17:59:43 +0200

    Réparation des conneries de ludo et design start activity

--- app/src/main/java/com/example/ludovic/zikub/StartActivity.java
+++ app/src/main/java/com/example/ludovic/zikub/StartActivity.java
@@ -12,15 +12,12 @@ public class StartActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
 
-        final Button button = (Button) findViewById(R.id.email_sign_in_button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent myIntent = new Intent(StartActivity.this, LoginActivity.class);
-                StartActivity.this.startActivity(myIntent);
-            }
-        });
+    /** Called when the user taps the Login button */
+    public void login(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
 
